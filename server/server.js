a78afd4d472cc2d371e7d2bd69ff73bf98002526
Joe Karlsson <josephcarlson1@gmail.com>
2018-02-28 15:45:07 -0600

    graphiql works!

--- server/server.js
+++ server/server.js
@@ -1,38 +1,26 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require ('body-parser');
-const path = require('path');
-const cors = require('cors');
-const ToDo = require ('./database/Todo');
-const { graphql } = require('graphql');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
+const path = require('path');
+const ToDo = require ('./database/Todo');
 const schema = require('./graphql/Schema');
-
-const SERVER_PORT = 3000;
 
 module.exports = function (app) {
 	app.use(bodyParser.urlencoded({extended:true}))
-	app.use(cors())
 
 	mongoose.connect('mongodb://database:27017/local')
-
 	const db = mongoose.connection;
 	db.on('error', ()=> { console.log( '---FAILED to connect to mongoose') })
 	db.once('open', () => {
 		console.log( '+++Connected to mongoose')
 	})
 
-	// app.head('/graphql', (req, res) => {
-	//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-	//   res.header('Access-Control-Request-Method', 'GET, POST');
-	//   res.header('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, Content-Length');
-	//   res.end();
-	// });
+	app.use('/', express.static(path.resolve(__dirname, 'dist')));
 
-	app.use('/graphql', graphqlHTTP (req => ({
-		schema,
-		graphiql:true,
-	})))
+	app.use('/graphql', graphqlHTTP({
+	  schema: schema,
+	  graphiql: true
+	}));
 
 	app.post('/quotes',(req,res)=>{
 		// Insert into TodoList Collection
@@ -50,10 +38,5 @@ module.exports = function (app) {
 		})
 	})
 
-	app.use('/', express.static(path.resolve(__dirname, 'dist')));
-
-
-	app.listen(SERVER_PORT, () => {
-	  console.log(`Server is now running on http://localhost:${SERVER_PORT}`);
-	});
+	app.listen(3000);
 }
