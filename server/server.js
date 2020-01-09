3c3346524bc199dfd430b11d34cd82e6e680cb30
Joe Karlsson <joe.carlson@callmejoe.net>
2020-01-09 09:43:31 -0600

    clean.

--- server/server.js
+++ server/server.js
@@ -1,50 +1,57 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require ('body-parser');
-const path = require('path');
-const ToDo = require ('./database/Todo');
-const { graphql } = require('graphql');
-const graphqlHTTP = require('express-graphql');
-const schema = require('./graphql/Schema');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+const ToDo = require("./database/Todo");
+const { graphql } = require("graphql");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./graphql/Schema");
 
 const app = express();
-const SERVER_PORT = 3000;
+const SERVER_PORT = 3001;
 
-module.exports = function (app) {
-	app.use(bodyParser.urlencoded({extended:true}))
+module.exports = function(app) {
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-	mongoose.connect('mongodb://database:27017/local')
+  mongoose.connect("mongodb://database:27017/local");
 
-	const db = mongoose.connection;
-	db.on('error', ()=> { console.log( '---FAILED to connect to mongoose') })
-	db.once('open', () => {
-		console.log( '+++Connected to mongoose')
-	})
+  const db = mongoose.connection;
+  db.on("error", () => {
+    console.log("---FAILED to connect to mongoose");
+  });
+  db.once("open", () => {
+    console.log("+++Connected to mongoose");
+  });
 
-	app.use('/', express.static(path.resolve(__dirname, 'dist')));
+  app.use("/", express.static(path.resolve(__dirname, "dist")));
 
-	app.use('/graphql', graphqlHTTP (req => ({
-		schema,
-		// graphiql:true,
-	})))
+  app.use(
+    "/graphql",
+    graphqlHTTP(req => ({
+      schema
+      // graphiql:true,
+    }))
+  );
 
-	app.post('/quotes',(req,res)=>{
-		// Insert into TodoList Collection
-		var todoItem = new ToDo({
-			itemId:1,
-			item:req.body.item,
-			completed: false
-		})
+  app.post("/quotes", (req, res) => {
+    // Insert into TodoList Collection
+    var todoItem = new ToDo({
+      itemId: 1,
+      item: req.body.item,
+      completed: false
+    });
 
-		todoItem.save((err,result)=> {
-			if (err) {console.log("---TodoItem save failed " + err)}
-			console.log("+++TodoItem saved successfully " + todoItem.item)
+    todoItem.save((err, result) => {
+      if (err) {
+        console.log("---TodoItem save failed " + err);
+      }
+      console.log("+++TodoItem saved successfully " + todoItem.item);
 
-			res.redirect('/')
-		})
-	})
+      res.redirect("/");
+    });
+  });
 
-	app.listen(SERVER_PORT, () => {
-	  console.log(`Server is now running on http://localhost:${SERVER_PORT}`);
-	});
-}
+  app.listen(SERVER_PORT, () => {
+    console.log(`Server is now running on http://localhost:${SERVER_PORT}`);
+  });
+};
